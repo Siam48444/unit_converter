f535c2a04da146bce7c4140f92f61d81a74e7dba
Siam <siam48444@gmail.com>
2025-03-02 12:44:09 +0600

    siam

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { length_conversion_factors } from "./Conversion_Categories/length.js";
 
 
+const conversion_category_selector = document.getElementById("conversion_category_selector");
+const units_selector = document.querySelectorAll(".units_selector");
 
+const input_box = document.getElementById("input_box");
+const output_box = document.getElementById("output_box");
