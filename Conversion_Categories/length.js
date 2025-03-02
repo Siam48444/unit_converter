1824f12133a08991bd9ee9319ed0c1fe9c692d43
Siam <siam48444@gmail.com>
2025-03-02 12:35:05 +0600

    siam

---
+++ Conversion_Categories/length.js
@@ -0,0 +1,5 @@
+export const length_conversion_factors = {
+    kilometer: {
+        centimeter: 100000,
+    }
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+import { length_conversion_factors } from "./Conversion_Categories/length.js";
