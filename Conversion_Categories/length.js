51e73a198df29c3e3af71a5ce4de2ef71f00913e
Siam <siam48444@gmail.com>
2025-03-02 12:40:30 +0600

    siam

--- Conversion_Categories/length.js
+++ Conversion_Categories/length.js
@@ -1,5 +1,14 @@
 export const length_conversion_factors = {
-    kilometer: {
-        centimeter: 100000,
+    centimeter: {
+        centimeter: 1,
+        foot: 1 / 30.48,
+        inch: 1 / 2.54,
+        kilometer: 1 / 100000,
+        meter: 1 / 100,
+        micrometer: 1 / 10000,
+        mile: 1 / 160900,
+        millimeter: 10,
+        nanometer: 10000000,
+        yard: 1 / 91.44,
     }
 }
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 import { length_conversion_factors } from "./Conversion_Categories/length.js";
+
+
+
