ae89576db81abf81915939cf9b3ecc167a6bbd47
Siam <siam48444@gmail.com>
2025-03-02 12:54:03 +0600

    siam

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { length_conversion_factors } from "./Conversion_Categories/length.js";
 
 
+
+
 const conversion_category_selector = document.getElementById("conversion_category_selector");
 
 const input_unit_selector = document.getElementById("input_unit_selector");
-const output_unit_selector = document.getElementById("input_unit_selector");
+const output_unit_selector = document.getElementById("output_unit_selector");
 
 const input_box = document.getElementById("input_box");
 const output_box = document.getElementById("output_box");
@@ -18,7 +20,8 @@ output_box.addEventListener("input", () => { update_input(); });
 
 function update_input() {
     const input_unit = input_unit_selector.value;
+    const output_unit = output_unit_selector.value;
     const input_value = input_box.value;
 
-    console.log(input_unit)
+    console.log(output_unit)
 }
