65de163607db9cf4946c6cb7041564dffae9b53c
Siam <siam48444@gmail.com>
2025-03-02 13:02:49 +0600

    siam

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ output_box.addEventListener("input", () => { update_input(); });
 function update_input() {
     const input_unit = input_unit_selector.value;
     const output_unit = output_unit_selector.value;
-    const input_value = input_box.value;
 
-    const length_factor = length_conversion_factors[input_unit][output_unit];
-    output_box.value = input_value * length_factor;
+    const input_value = parseFloat(input_box.value);
+
+    // const length_factor = length_conversion_factors[input_unit][output_unit];
+
+    // output_box.value = input_value * length_factor;
+
+    console.log(length_conversion_factors[input_value])
 }
