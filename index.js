8a61ac775f6761278cc59122fc64240b360962d5
Siam <siam48444@gmail.com>
2025-03-02 12:53:01 +0600

    siam

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ import { length_conversion_factors } from "./Conversion_Categories/length.js";
 
 
 const conversion_category_selector = document.getElementById("conversion_category_selector");
-const units_selector = document.querySelectorAll(".units_selector");
+
+const input_unit_selector = document.getElementById("input_unit_selector");
+const output_unit_selector = document.getElementById("input_unit_selector");
 
 const input_box = document.getElementById("input_box");
 const output_box = document.getElementById("output_box");
+
+
+
+input_box.addEventListener("input", () => { update_input(); });
+output_box.addEventListener("input", () => { update_input(); });
+
+
+
+function update_input() {
+    const input_unit = input_unit_selector.value;
+    const input_value = input_box.value;
+
+    console.log(input_unit)
+}
