abc243564628325b5d0627e36d7b2c08e79efa69
Siam <siam48444@gmail.com>
2025-03-03 12:05:46 +0600

    siam

--- Conversion_Categories/area.js
+++ Conversion_Categories/area.js
@@ -1,3 +1,12 @@
 export const area_conversion_factors = {
-    
+    acre: {
+        acre: 1,
+        square_foot: 43560,
+        hectare: 1 / 2.471,
+        square_inch: 6.273e+6,
+        square_kilometer: 1 / 247.1,
+        square_meter: 4046.86,
+        square_mile: 1 / 640,
+        square_yard: 4840,
+    },
 } 
