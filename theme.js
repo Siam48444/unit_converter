8c3c80a6211feb9f1613813ea407efed908e82ba
Siam <siam48444@gmail.com>
2025-03-05 12:22:27 +0600

    siam

--- theme.js
+++ theme.js
@@ -1,5 +1,7 @@
-const nav = document.getElementById("nav");
-const main = document.getElementById("main");
-
 // Adjust the heights of different sections
-main.style.height = `${100 - nav.clientHeight}vh`;
+window.addEventListener("DOMContentLoaded", () => {
+    const nav = document.getElementById("nav");
+    const main = document.getElementById("main");
+
+    main.style.height = `${window.innerHeight - nav.clientHeight}px`;
+});    
