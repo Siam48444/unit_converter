02429ecabf7784cbf22ecc10a29848c975862c50
Siam <siam48444@gmail.com>
2025-03-05 12:46:23 +0600

    siam

--- theme.js
+++ theme.js
@@ -4,14 +4,16 @@ window.addEventListener("resize", adjustHeights); // Adjust heights whenever the
 
 // Function to adjust the heights of different sections
 function adjustHeights() {
-  const nav = document.getElementById("nav");
-  const main = document.getElementById("main");
-  const unit_converter_app = document.getElementById("unit_converter_app");
+    const nav = document.getElementById("nav");
+    const main = document.getElementById("main");
+    const unit_converter_app = document.getElementById("unit_converter_app");
 
-  main.style.height = `${window.innerHeight - nav.offsetHeight}px`;
+    // Handle the edge cases
+    if (unit_converter_app.getBoundingClientRect().top === nav.offsetHeight * 2) {
+        alert();
+        // main.style.height = "auto";
+    }
 
-  // Handle the edge cases
-  if (true) {
-    main.style.height = "auto";
-  }
+    // Adjust the main hright
+    main.style.height = `${window.innerHeight - nav.offsetHeight}px`;
 }
