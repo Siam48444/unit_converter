d65ac804ea73ff919d2690d13711fea48d907dfc
Siam <siam48444@gmail.com>
2025-03-06 13:11:25 +0600

    siam

--- theme.js
+++ theme.js
@@ -1,13 +1,22 @@
-window.addEventListener("DOMContentLoaded", adjust_heights); // Adjust heights when the page loads
-window.addEventListener("resize", adjust_heights); // Adjust heights whenever the window is resized
+const nav = document.getElementById("nav");
+const main = document.getElementById("main");
+const whole_container = document.getElementById("whole_container");
+const unit_converter_app = document.getElementById("unit_converter_app");
+
+
+// Adjust heights whenever the window is resized
+window.addEventListener("resize", adjust_heights); 
+
+// Adjust heights when the page loads
+window.addEventListener("DOMContentLoaded", () => {
+    adjust_heights();
+    fade_animation();
+}); 
+
 
 
 // Function to adjust the heights of different sections
 function adjust_heights() {
-    const nav = document.getElementById("nav");
-    const main = document.getElementById("main");
-    const unit_converter_app = document.getElementById("unit_converter_app");
-
     // Adjust the main height
     main.style.height = `${window.innerHeight - nav.clientHeight}px`;
 
@@ -15,4 +24,11 @@ function adjust_heights() {
     if (unit_converter_app.getBoundingClientRect().top < nav.clientHeight * 2.2) {
         main.style.height = "auto";
     }
+}
+
+
+
+// Fade animation after the page loads
+function fade_animation() {
+    whole_container.style.opacity = "1";
 }
