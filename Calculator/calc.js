4365160fe860757ca912569f9e3ecfcd950f0ae6
Siam <siam48444@gmail.com>
2025-04-23 22:18:35 +0600

    siam

---
+++ Calculator/calc.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const allClearButton = document.querySelector('[data-ac]');
+const deleteButton = document.querySelector('[data-delete]');
+const percentButton = document.querySelector('[data-percent]');
+
+const previous = document.querySelector('[data-previous]');
+const current = document.querySelector('[data-current]');
